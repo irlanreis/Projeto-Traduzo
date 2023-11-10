158be1c06eda75adc3d69464189955a66c4b0f3f
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-11-09 23:28:49 -0400

    Feat: Requisito 09

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,6 +1,23 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
 
 
 def test_history_delete(app_test):
-    raise NotImplementedError
+    history_one = HistoryModel(
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        }
+    ).save()
+
+    UserModel({"name": "xabloso", "token": "123456"}).save()
+    user = UserModel.find_one({"name": "xabloso"})
+
+    app_test.delete(
+        f"/admin/history/{history_one.data['_id']}",
+        headers={
+            "Authorization": user.data["token"],
+            "User": user.data["name"],
+        },
+    )
