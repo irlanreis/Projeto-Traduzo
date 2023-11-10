a275d1b73b272def3a0cdabe115cba648232c147
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-11-09 22:24:59 -0400

    Fix: Correções de Bugs

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+
 from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 from models.history_model import HistoryModel
@@ -10,30 +11,43 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    languages = LanguageModel.list_dicts()
-    text_to_translate = request.form.get("text-to-translate") or ""
-    translate_from = request.form.get("translate-from") or "pt"
-    translate_to = request.form.get("translate-to") or "en"
-    translator_google = GoogleTranslator(
-        source="auto", target=translate_to
-    ).translate(text_to_translate)
-    translated = translator_google or ""
+    if request.method == "GET":
+        return render_template(
+            "index.html",
+            languages=LanguageModel.list_dicts(),
+            text_to_translate="O que deseja traduzir?",
+            translate_from="pt",
+            translate_to="en",
+            translated="What do you want to translate?",
+        )
+    elif request.method == "POST":
+        languages = LanguageModel.list_dicts()
 
-    history_dict = {
-        "text_to_translate": text_to_translate,
-        "translate_from": translate_from,
-        "translate_to": translate_to,
-    }
-    HistoryModel(history_dict).save()
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
 
-    return render_template(
-        "index.html",
-        languages=languages,
-        text_to_translate=text_to_translate,
-        translate_from=translate_from,
-        translate_to=translate_to,
-        translated=translated,
-    )
+        translator_google = GoogleTranslator(
+            source="auto", target=translate_to
+        ).translate(text_to_translate)
+
+        translated = translator_google
+
+        history_dict = {
+            "text_to_translate": text_to_translate,
+            "translate_from": translate_from,
+            "translate_to": translate_to,
+        }
+        HistoryModel(history_dict).save()
+
+        return render_template(
+            "index.html",
+            languages=languages,
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
+        )
 
 
 # Req. 6
